061bd97c349855aff03056144ebe10d575abcc27
Nathan <nathannhek@csu.fullerton.edu>
2019-07-30 20:48:38 -0700

    Add Nav bar and redirect on submit

--- scripts/register.js
+++ scripts/register.js
@@ -22,9 +22,24 @@ function myFunction(e) {
         .then(res => {
             console.log(res);
         });
-        e.preventDefault();
+        e.preventDefault();   
 }
 
 let form = document.getElementById('form');
-console.log(form);
 form.addEventListener('submit', myFunction);
+
+document.getElementById("submitbtn").onclick = function () {
+    location.href = "/site/login.html";
+};
+
+// Search Users
+const store = new SteinStore(
+  "https://api.steinhq.com/v1/storages/5d3f9f9b87c49c04cac13690"
+);
+
+store.read("Users", { search: { fname: "poo23" } }).then(data => {
+//   console.log(data[0].fname);
+  var x = [...data];
+  console.log(x);
+  console.log(x[0].fname);  
+});
