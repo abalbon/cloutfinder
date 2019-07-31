765cd576da3c230d636f47eb61006906e4933904
Albert Balbon <ajbalbon@gmail.com>
2019-07-30 17:43:39 -0700

    still not working but somehow still have hope

---
+++ cloutfinder.js
@@ -0,0 +1,25 @@
+let index = 0;
+
+function nextUser() {
+    console.log("Button is pressed");
+    index = index + 1;
+
+    document.getElementById("likeChoice").value = 
+    
+    //index = $(".headerText").data("stein-offset") + 1;
+    $(".headerText").attr("data-stein-offset", index); 
+    
+    console.log(index);
+
+}
+
+
+
+
+
+ 
+
+
+
+
+    
