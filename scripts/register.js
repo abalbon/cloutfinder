050223bdd3ffd4289a8982b258f186f481b4e6e0
Albert Balbon <ajbalbon@gmail.com>
2019-07-31 21:20:05 -0700

    working stuff

--- scripts/register.js
+++ scripts/register.js
@@ -1,14 +1,29 @@
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  } 
+
 function myFunction(e) {
 
     const store = new SteinStore(
         "https://api.steinhq.com/v1/storages/5d3f9f9b87c49c04cac13690"
     );
+
+    //let images = ["/img/user1.jpg", "/img/user2.jpg", "/img/user3.jpg"];
+    //let randNum = Math.floor(Math.random() * 3);
+
     let fname = $("#fname").val();
     let lname = $("#lname").val();
     let username = $("#username").val();
     let email = $("#email").val();
     let password = $("#psw").val();
-    
+    let uuid = uuidv4();
+    let img = "/img/user1.jpg";
+
+
+     
     store
         .append("Users", [
         {
@@ -16,7 +31,9 @@ function myFunction(e) {
             lname: lname,
             username: username,
             email: email,
-            password: password
+            password: password,
+            id: uuid,
+            image: img
         }
         ])
         .then(res => {
@@ -45,3 +62,4 @@ store.read("Users", { search: { fname: "poo23" } }).then(data => {
 });
 
 console.log("arr: " + arr);
+
